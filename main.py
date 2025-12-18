7ede7e2e6b52bc996842cfc8d1c0b833387727be
Siqi Yan <syan235711@gmail.com>
2025-12-18 21:50:10 +0000

    style: better

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 try:
-    amount = int(input("Enter the shift amount: "))
+    amount = int(input('Enter the shift amount: '))
 except ValueError:
-    print("Invalid input. Please enter an integer.")
+    print('Invalid input. Please enter an integer.')
     exit(1)
 
-text = input("Enter a text:").lower()
-result = ""
+text = input('Enter a text:').lower()
+result = ''
 for i, char in enumerate(text):
     if char not in alphabet:
         result += char
